7377800393b07d66b0f57846c85a262276586539
dadayan188 <87197207+dadayan188@users.noreply.github.com>
2022-03-14 04:18:25 +0300

    Add files via upload

---
+++ fact/weather/src/index.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+
+  const mainWindow = new BrowserWindow({
+    width: 400,
+    height: 600,
+    resizable: false,
+    autoHideMenuBar: true,
+    icon: './hot.ico',
+  })
+
+  mainWindow.loadFile('./src/index.html')
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ fact/weather/src/script.js
@@ -0,0 +1,62 @@
+let tempBlock = document.querySelector('#temp')
+let cityBlock = document.querySelector('#city')
+let imgBlock = document.querySelector('.img-block')
+let update_date = document.querySelector('#update-date')
+let local_date = document.querySelector('#local-date')
+let searchInp = document.querySelector('.search')
+
+setInterval(() => {
+    let date = new Date;
+    local_date.textContent = `Local time: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}, 1000)
+
+let city = 'London'
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        let value = searchInp.value;
+        if(!value) return false;
+        city = value;
+        init()
+        searchInp.value = ''
+    }
+})
+
+function init() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d982b206b7125a363d94918d08ebf560`)
+    .then((resp) => {return resp.json()})
+    .then((data) => {
+
+
+        tempBlock.textContent = `${temperature()}°`
+
+        cityBlock.textContent = `City: ${data.name}`
+
+        console.log()
+
+        function temperature() {
+            let getTemp = data.main.temp
+            let tempC = Math.floor(getTemp) - 273
+            return tempC
+        }
+
+        let date = new Date;
+
+        update_date.textContent = `Update time: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+        console.log('перезапуск')
+    })
+    .catch(() => {
+        alert('Такого нету, попробуй еще разок')
+        city = 'London';
+        init()
+        searchInp.value = ''
+    })
+}
+
+init()
+
+setInterval(() => {
+    init()
+}, 10000)
+
